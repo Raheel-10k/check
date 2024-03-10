69411a9e062be38486da5890ba9195131995ba4c
Raheel Kotwal <2023.raheelk@isu.ac.in>
2024-03-10 22:24:26 +0530

    max Consecutive Ones Corrected

--- java/maxConsecutiveOnes.java
+++ java/maxConsecutiveOnes.java
@@ -1,16 +1,21 @@
-class Solution {
+public class maxConsecutiveOnes {
     public int findMaxConsecutiveOnes(int[] nums) {
-        int count=0;
-        for(int i=0;i<nums.length;i++) {
-            if(nums[i]==1){
-                if(nums[i]==nums[i+1]) {
-                    count++;
-                }
-                else {
-                    count=0;
-                }
-            }
+        int maxCount = 0;
+        int current = 0;
+
+        for (int x : nums) {
+            current = (x == 1) ? current + 1 : 0;
+            maxCount = Math.max(maxCount, current);
         }
-        return count;
+
+        return maxCount;
+    }
+
+    public static void main(String[] args) {
+        maxConsecutiveOnes solution = new maxConsecutiveOnes();
+        int[] nums1 = {1, 1, 0, 1, 1, 1};
+        System.out.println("Example 1: " + solution.findMaxConsecutiveOnes(nums1));
+        int[] nums2 = {1, 0, 1, 1, 0, 1};
+        System.out.println("Example 2: " + solution.findMaxConsecutiveOnes(nums2));
     }
 }
